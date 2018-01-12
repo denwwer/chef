cab2c9385e359ab086a08e7425448bf5f3dcc0c5
boris <denwwer.c4@gmail.com>
2018-01-12 20:31:44 +0200

    add new monit conf; minor updates

--- chef/ufw/chef.sh
+++ chef/ufw/chef.sh
@@ -7,11 +7,11 @@ function chef_ufw {
 	title 'Configure UFW'
 
 	# More on https://www.digitalocean.com/community/tutorials/ufw-essentials-common-firewall-rules-and-commands
-	sudo ufw allow 22      # ssh
-	sudo ufw allow 80      # http
-	sudo ufw allow 443     # https
-	sudo ufw allow 2812    # monit
-	sudo ufw allow out 587 # SMTP
+	sudo ufw allow 22      # SSH
+	sudo ufw allow 80      # HTTP
+	sudo ufw allow 443     # HTTPS
+	sudo ufw allow 2812    # Monit
+	sudo ufw allow out 587 # SMTP RFC
 	sudo ufw allow out 25  # SMTP
 	sudo ufw default deny
 }
